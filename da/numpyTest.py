05ba54b8f3190d8bc37ef37cba554bbb781c8706
383817938@qq.com <thankyour123>
2018-03-20 01:02:15 +0800

    ADD:0.0.1

--- da/numpyTest.py
+++ da/numpyTest.py
@@ -1,6 +1,23 @@
+#
 import numpy as np
 
 if __name__ == "__main__":
     print("name")
-    print(np.arange(10))
-    pass
+    print(np.arange(20))
+    range = np.arange(20)
+    reshape = range.reshape(4, 5)
+    print(reshape)
+    print(np.arange(20).reshape(4, 5))
+    t = np.dtype([('symbol', str, 40), ('open', float), ('close', float)])
+    itemz = np.array([('btcusdt', 43.2, 3.14), ('htusdt', 2.3, 2.5)],dtype=t)
+    print(t)
+    print(itemz[1]["symbol"])
+    print(itemz[0])
+    print(range[::5])
+    print(itemz["open"])
+    print(itemz.ravel())#
+    a=np.arange(20).reshape(4,5)
+    b=np.hsplit(a,5)
+    print(np.array(np.arange(5)).tolist())
+    np.loadtxt()
+    # print(b)
